54b1f71788b74000de1e1d416c367b4b2155cbaa
prasouza <prasouza@gmail.com>
2025-01-08 13:18:16 -0300

    v1

--- app/index.js
+++ app/index.js
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 import api from './services/api';
-import Filmes from './Filmes'
+import Filmes from './Filmes';
+
 export default function Index() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
       const response = await api.get('r-api/?api=filmes');
       setFilmes(response.data);
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={filmes}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <Filmes data={item}/>}
-                
-      />
-    </View>
-  );
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator color="#121212" size={45} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={filmes}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Filmes data={item} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
